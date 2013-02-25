39aa6f5a7c3927fdb48ee9c34e04f938ab05aa0c
Keith <keith.strand@gmail.com>
2013-02-24 19:30:55 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Favstar::Application.config.secret_token = '9a23ab04f0891161bbf34d42ea29a96d3c328f9acf134ec6de5f410d61c4e395f4d10c90111596db06e576035c54a8788b62ee3d9e66ff11c46bdcc12d03ae51'
